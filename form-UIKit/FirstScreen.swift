e1592e92abaa4ee9da44bf7834d74d2ade61cb49
tandyys <tansabana12@gmail.com>
2025-09-29 16:07:32 +0700

    Refactor
    Create reusable set up functions

--- form-UIKit/FirstScreen.swift
+++ form-UIKit/FirstScreen.swift
@@ -9,104 +9,139 @@ import UIKit
 
 class FirstScreen: UIViewController {
     let button: UIButton = UIButton()
+    let button2: UIButton = UIButton()
     let input: UITextField = UITextField()
+    let input2: UITextField = UITextField()
     let featureTitle: UILabel = UILabel()
+    let featureTitle2: UILabel = UILabel()
+    let resultHstackView: UIStackView = UIStackView()
+    let resultHstackView2: UIStackView = UIStackView()
     let resultLabel: UILabel = UILabel()
+    let resultLabel2: UILabel = UILabel()
     let resultValueLabel: UILabel = UILabel()
-    
+    let resultValueLabel2: UILabel = UILabel()
+    let spacer: UIView = UIView()
+    let spacer2: UIView = UIView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpLabel()
-        setUpInputTextField()
-        setUpExecuteButton()
-        setUpResultLabel()
+        setUpLabel(featureTitle, "Palindrome Checker", view.layoutMarginsGuide.leadingAnchor, view.layoutMarginsGuide.trailingAnchor, view.layoutMarginsGuide.topAnchor, 0)
+        setUpInputTextField(input, "Enter a word you want to check", view.layoutMarginsGuide.leadingAnchor, view.layoutMarginsGuide.trailingAnchor, featureTitle.bottomAnchor, 25, 50)
+        setUpExecuteButton(button, .systemPink, #selector(didTapPalindromeButton), "Check", input.bottomAnchor)
+        setUpResultLabel(resultLabel, resultValueLabel, resultHstackView, spacer, button.bottomAnchor)
+        setUpLabel(featureTitle2, "Valid Parentheses", view.layoutMarginsGuide.leadingAnchor, view.layoutMarginsGuide.trailingAnchor, resultHstackView.bottomAnchor, 50)
+        setUpInputTextField(input2, "Enter brackets pair here to check", view.layoutMarginsGuide.leadingAnchor, view.layoutMarginsGuide.trailingAnchor, featureTitle2.bottomAnchor, 25, 50)
+        setUpExecuteButton(button2, .systemPurple, #selector(didTapValidParenthesesButton), "Check", input2.bottomAnchor)
+        setUpResultLabel(resultLabel2, resultValueLabel2, resultHstackView2, spacer2, button2.bottomAnchor)
+
         view.backgroundColor = .systemBackground
     }
     
-    func setUpLabel() {
-        view.addSubview(featureTitle)
+    func setUpLabel(_ l: UILabel, _ t: String, _ s: NSLayoutXAxisAnchor, _ e: NSLayoutXAxisAnchor, _ ta: NSLayoutYAxisAnchor, _ c: CGFloat) {
+        view.addSubview(l)
         
-        featureTitle.text = "Palindrome Checker"
-        featureTitle.font = .systemFont(ofSize: 30, weight: .bold)
-        featureTitle.textAlignment = .left
-        featureTitle.translatesAutoresizingMaskIntoConstraints = false
+        l.text = t
+        l.font = .systemFont(ofSize: 30, weight: .bold)
+        l.textAlignment = .left
+        l.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            featureTitle.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            featureTitle.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            featureTitle.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
+            l.leadingAnchor.constraint(equalTo: s),
+            l.trailingAnchor.constraint(equalTo: e),
+            l.topAnchor.constraint(equalTo: ta, constant: c)
         ])
     }
     
-    func setUpInputTextField() {
-        view.addSubview(input)
+    func setUpInputTextField(_ v: UITextField, _ p: String, _ l: NSLayoutXAxisAnchor, _ t: NSLayoutXAxisAnchor, _ ta: NSLayoutYAxisAnchor, _ ct: CGFloat, _ ch: CGFloat) {
+        view.addSubview(v)
         
-        input.placeholder = "Enter word you want to check"
-        input.borderStyle = .roundedRect
-        input.clearButtonMode = .whileEditing
-        input.returnKeyType = .done
-        input.autocorrectionType = .no
-        input.autocapitalizationType = .none
-        input.textContentType = .none
+        v.placeholder = p
+        v.borderStyle = .roundedRect
+        v.clearButtonMode = .whileEditing
+        v.returnKeyType = .done
+        v.autocorrectionType = .no
+        v.autocapitalizationType = .none
+        v.textContentType = .none
         
-        input.translatesAutoresizingMaskIntoConstraints = false
+        v.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            input.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            input.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            input.topAnchor.constraint(equalTo: featureTitle.bottomAnchor, constant: 25),
-            input.heightAnchor.constraint(equalToConstant: 50)
+            v.leadingAnchor.constraint(equalTo: l),
+            v.trailingAnchor.constraint(equalTo: t),
+            v.topAnchor.constraint(equalTo: ta, constant: ct),
+            v.heightAnchor.constraint(equalToConstant: ch)
         ])
     }
     
-    func setUpExecuteButton() {
-        view.addSubview(button)
+    func setUpExecuteButton(_ b: UIButton, _ c: UIColor, _ sf: Selector, _ t: String, _ ta: NSLayoutYAxisAnchor) {
+        view.addSubview(b)
         
-        button.configuration = .filled()
-        button.configuration?.baseBackgroundColor = .systemPink
-        button.configuration?.title = "Execute"
-        button.translatesAutoresizingMaskIntoConstraints = false
+        b.configuration = .filled()
+        b.configuration?.baseBackgroundColor = c
+        b.configuration?.title = t
+        b.translatesAutoresizingMaskIntoConstraints = false
         
-        //what will button do when tapped
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        b.addTarget(self, action: sf, for: .touchUpInside)
         
         NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.topAnchor.constraint(equalTo: input.bottomAnchor, constant: 50),
-            button.widthAnchor.constraint(equalToConstant: 200),
-            button.heightAnchor.constraint(equalToConstant: 50)
+            b.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            b.topAnchor.constraint(equalTo: ta, constant: 25),
+            b.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            b.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
     
-    func setUpResultLabel() {
-        view.addSubview(resultLabel)
-        view.addSubview(resultValueLabel)
+    func setUpResultLabel(_ rl: UILabel, _ rvl: UILabel, _ sv: UIStackView, _ s: UIView, _ b: NSLayoutYAxisAnchor) {
+        sv.addArrangedSubview(rl)
+        sv.addArrangedSubview(rvl)
+        sv.addArrangedSubview(s)
+        view.addSubview(sv)
         
-        resultLabel.text = "Result: "
-        resultLabel.font = .systemFont(ofSize: 20, weight: .bold)
-        resultLabel.textAlignment = .left
-        resultLabel.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .horizontal
+        sv.alignment = .center
+        sv.distribution = .equalSpacing
+        sv.spacing = 10
+        sv.translatesAutoresizingMaskIntoConstraints = false
         
-        resultValueLabel.text = "-"
-        resultValueLabel.font = .systemFont(ofSize: 20)
-        resultValueLabel.textAlignment = .left
-        resultValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        rl.text = "Result: "
+        rl.font = .systemFont(ofSize: 20, weight: .bold)
+        rl.textAlignment = .left
+        rl.translatesAutoresizingMaskIntoConstraints = false
+        
+        rvl.text = "-"
+        rvl.font = .systemFont(ofSize: 20)
+        rvl.textAlignment = .left
+        rvl.translatesAutoresizingMaskIntoConstraints = false
+        
+        s.backgroundColor = .purple
+        s.translatesAutoresizingMaskIntoConstraints = false
+//        spacer.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+//        spacer.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
         
         NSLayoutConstraint.activate([
-            // Place the title label below the button
-            resultLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            resultLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            resultLabel.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 50),
-
-            // Place the value label below the title label
-            resultValueLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            resultValueLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            resultValueLabel.topAnchor.constraint(equalTo: resultLabel.bottomAnchor, constant: 10)
+            sv.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            sv.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            sv.topAnchor.constraint(equalTo: b, constant: 25),
+            rl.widthAnchor.constraint(equalTo: sv.widthAnchor, multiplier: 0.25),
+            rvl.widthAnchor.constraint(equalTo: sv.widthAnchor, multiplier: 0.25),
+            s.widthAnchor.constraint(equalTo: sv.widthAnchor, multiplier: 0.5)
         ])
+        
+//        NSLayoutConstraint.activate([
+//            // Place the title label below the button
+//            resultLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+//            resultLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+//            resultLabel.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 50),
+//
+//            // Place the value label below the title label
+//            resultValueLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+//            resultValueLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+//            resultValueLabel.topAnchor.constraint(equalTo: resultLabel.bottomAnchor, constant: 10)
+//        ])
     }
     
-    @objc func didTapButton() {
+    @objc func didTapPalindromeButton() {
         let userInput: String = input.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
         guard !userInput.isEmpty else {
             resultValueLabel.text = "-"
@@ -117,9 +152,40 @@ class FirstScreen: UIViewController {
         print(res)
     }
     
+    @objc func didTapValidParenthesesButton() {
+        let userInput: String = input2.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        guard !userInput.isEmpty else {
+            resultValueLabel2.text = "-"
+            return
+        }
+        let res: String = isValidParent(userInput)
+        resultValueLabel2.text = res
+        print(res)
+    }
+    
     private func isPalindrome(_ s: String) -> String {
         let tempS = s.reversed()
         if s == String(tempS) { return "True" } else { return "False" }
+    }
+    
+    private func isValidParent(_ s: String) -> String {
+        let map: [Character:Character] = [
+            ")":"(",
+            "]":"[",
+            "}":"{"
+        ]
+        var tempStack: [Character] = []
+        
+        for c in s {
+            if map.values.contains(c) {
+                tempStack.append(c)
+            } else if tempStack.last == map[c] && !tempStack.isEmpty {
+                tempStack.removeLast()
+            } else {
+                return "False"
+            }
+        }
+        return tempStack.isEmpty ? "True" : "False"
     }
 }
 
